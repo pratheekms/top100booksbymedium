6a59b42385821053b573f1b68146fc07504525b7
pratheek <ms.pratheek1@gmail.com>
2020-01-12 23:23:58 +0530

    main alter for scrapping

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import webScrapping
 
 def main():
     start = time.time()
-    url="https://medium.com/world-literature/creating-the-ultimate-list-100-books-to-read-before-you-die-45f1b722b2e5"
-    webScrapping.scrapingFunction(url,"strong","id ke")
-
+    url = "https://medium.com/world-literature/creating-the-ultimate-list-100-books-to-read-before-you-die-45f1b722b2e5"
+    webScrapping.scrapingFunction(url, "strong", "id ke")
 
     end = time.time()
     print("time taken by program is:" + str(end - start))
