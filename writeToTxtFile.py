818094a2b4799688d550e854c99953534f989a9c
pratheek <ms.pratheek1@gmail.com>
2020-01-12 22:38:01 +0530

    function 7

--- writeToTxtFile.py
+++ writeToTxtFile.py
@@ -1,11 +1,13 @@
 import openpyxl
-def writeToTxtFileFunction(rowNum,colNum,value):
+
+
+def writeToTxtFileFunction(rowNum, colNum, value):
     wb_obj = openpyxl.load_workbook("top100BooksByMedium.xlsx")
     sheet_obj = wb_obj.active
     m_row = sheet_obj.max_row
     print("rows=" + str(m_row))
 
-    sheet_obj.cell(row=rowNum + 1, column=colNum).value = str(value) #col=4
+    sheet_obj.cell(row=rowNum + 1, column=colNum).value = str(value)  # col=4
     print("---rating write complete---")
     print("-----------------------")
     wb_obj.save("top100BooksByMedium.xlsx")
